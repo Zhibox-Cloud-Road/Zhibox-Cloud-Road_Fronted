3a00bc1eae194d8d4a9aa86404ea7d7c201bf1ab
Fuyj <2312696405@qq.com>
2020-10-20 06:11:21 +0800

    logo update

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -20,7 +20,8 @@
 
       <el-dropdown class="avatar-container right-menu-item hover-effect" trigger="click">
         <div class="avatar-wrapper">
-          <img :src="avatar+'?imageView2/1/w/80/h/80'" class="user-avatar">
+          <!-- <img :src="avatar+'?imageView2/1/w/80/h/80'" class="user-avatar"> -->
+          <img src="https://ftp.bmp.ovh/imgs/2020/10/aceaf4679badcfe8.png" class="user-avatar">
           <i class="el-icon-caret-bottom" />
         </div>
         <el-dropdown-menu slot="dropdown">
